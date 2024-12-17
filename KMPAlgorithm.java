5b8c9bcae9450a9cf8330315cdfd2c8685ae07fe
Mohammed Saqib <mohammedsaqib1706@gmail.com>
2024-12-17 22:44:01 +0530

    Day 18 -KMP Algorithm

--- KMPAlgorithm.java
+++ KMPAlgorithm.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class KMPAlgorithm {
+
+    ArrayList<Integer> search(String pat, String txt) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int index = txt.indexOf(pat);
+
+        while (index != -1) {
+            result.add(index);
+            index = txt.indexOf(pat, index + 1);
+        }
+
+        return result;
+    }
 }

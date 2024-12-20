26a15df01b3cadfc40f72cf4b0a86348aeeea620
Mohammed Saqib <mohammedsaqib1706@gmail.com>
2024-12-20 22:27:11 +0530

    Day 21 - Sort012s

---
+++ Sort012s.java
@@ -0,0 +1,32 @@
+public class Sort012s {
+
+    public void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    // Swap arr[low] and arr[mid], then increment low and mid
+                    int temp0 = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp0;
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    // Move mid pointer
+                    mid++;
+                    break;
+
+                case 2:
+                    // Swap arr[mid] and arr[high], then decrement high
+                    int temp2 = arr[high];
+                    arr[high] = arr[mid];
+                    arr[mid] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+}

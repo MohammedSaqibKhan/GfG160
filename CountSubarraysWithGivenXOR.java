748b0ee4e319747026580319d482531ceee9484d
Mohammed Saqib <mohammedsaqib1706@gmail.com>
2025-01-18 22:42:14 +0530

    Count Subarrays with given XO

---
+++ CountSubarraysWithGivenXOR.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountSubarraysWithGivenXOR {
+
+    public long subarrayXor(int arr[], int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int curr = 0;
+        long count = 0;
+        map.put(0, 1);
+        for(int num: arr) {
+            curr ^= num;
+
+            int target = curr ^ k;
+            if(map.containsKey(target)) {
+                count += map.get(target);
+            }
+
+            map.put(curr, map.getOrDefault(curr, 0) + 1);
+        }
+
+        return count;
+    }
+}
